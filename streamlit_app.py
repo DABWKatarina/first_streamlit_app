88b67d453f8d258a72b9cab3e7739056d9f35bf8
DABWKatarina <137181815+DABWKatarina@users.noreply.github.com>
2023-06-20 15:23:48 +0200

    Update streamlit_app.py
    popravek

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Dineer')
 
-streamlit.subtitle('Breakfast Menu')
-streamlit.body('Omega 3 & Blueberry Oatmeal')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale,  3 Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Ragne Egg')
