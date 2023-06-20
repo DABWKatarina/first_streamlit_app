00acac7253c3de80f2a1ca90ab75e88d9efbd062
DABWKatarina <137181815+DABWKatarina@users.noreply.github.com>
2023-06-20 15:22:06 +0200

    Update streamlit_app.py
    Added subtitle and breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dineer')
+
+streamlit.subtitle('Breakfast Menu')
+streamlit.body('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale,  3 Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Ragne Egg')
