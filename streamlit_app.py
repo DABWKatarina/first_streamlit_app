2980dd720a0f2ac40b5f18b72684a079d9e2b019
DABWKatarina <137181815+DABWKatarina@users.noreply.github.com>
2023-06-22 00:33:16 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-import URLError from urllib.error
+import URLError #from urllib.error
 
 streamlit.title('My Mom\'s New Healthy Dineer')
 
